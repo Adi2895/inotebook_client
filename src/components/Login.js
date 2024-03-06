1f1d310087f425056a570405b43aa6810def5253
Adi2895 <jangraaditya11@gmail.com>
2024-03-06 16:43:25 +0530

    done

--- src/components/Login.js
+++ src/components/Login.js
@@ -36,9 +36,8 @@ const Login = (props) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // const response = await fetch(`${baseUrl}/api/auth/login`, {
-      const response = await fetch(`http://localhost:1024/api/auth/login`, {
-   
+    const response = await fetch(`${baseUrl}/api/auth/login`, {
+    
    
     method: "POST",
       headers: {
