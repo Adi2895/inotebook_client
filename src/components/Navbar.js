a8ebafa6eb4f479e5fba76c13b247d69bedfe8c2
Aditya jangra <jangraaditya11@gmail.com>
2024-03-14 19:35:41 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,7 +18,7 @@ export default function Navbar(props) {
         <>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">NoteSphere</Link>
+                    <Link className="navbar-brand" to="/">NoteEazy</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
